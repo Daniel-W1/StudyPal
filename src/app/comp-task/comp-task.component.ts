ec73090416efcf7993ec1466b13cebcacbb88ff9
Ananiya <ananiyat24@gmail.com>
2023-02-13 19:25:33 +0300

    completed the tasks page

---
+++ src/app/comp-task/comp-task.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp-task',
+  templateUrl: './comp-task.component.html',
+  styleUrls: ['./comp-task.component.css']
+})
+export class CompTaskComponent {
+
+}
